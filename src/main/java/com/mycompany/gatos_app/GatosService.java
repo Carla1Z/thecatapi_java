f9ddaa0a1d9557dda1366b6e902e14958f66074d
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-23 01:00:04 -0300

    Listando datos de APIs en la aplicación

--- src/main/java/com/mycompany/gatos_app/GatosService.java
+++ src/main/java/com/mycompany/gatos_app/GatosService.java
@@ -4,12 +4,57 @@
  */
 package com.mycompany.gatos_app;
 
+import com.google.gson.Gson;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.awt.Image;
+
+
 /**
  *
  * @author Carla
  */
 public class GatosService {
-    public static void verGatos(){
+    public static void verGatos() throws IOException{
+        //1. vamos a traer los datos de la api
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url("https://api.thecatapi.com/v1/images/search").get().build();
+
+        Response response = client.newCall(request).execute();
         
+        String elJson = response.body().string();
+        
+        //Cortar los corchetes
+        elJson = elJson.substring(1, elJson.length());
+        elJson = elJson.substring(0, elJson.length()-1);
+        
+        //crear un objeto de la clase Gatos
+        Gson gson = new Gson();
+        Gatos gatos = gson.fromJson(elJson, Gatos.class);
+        
+        //Redimensionar en caso de necesitar
+        Image image = null;
+        try{
+         URL url = new URL(gatos.getUrl());   
+         image = ImageIO.read(url);
+         
+        ImageIcon fondoGato = new ImageIcon(image);
+        
+        if(fondoGato.getIconWidth() > 800){
+            // redimensionamos
+            Image fondo = fondoGato.getImage();
+            Image modificada = fondo.getScaledInstance(800, 600, java.awt.Image.SCALE_SMOOTH);
+            fondoGato = new ImageIcon(modificada);
+        }
+        
+        }catch(IOException e){
+            System.out.println(e);
+        }
     }
 }
